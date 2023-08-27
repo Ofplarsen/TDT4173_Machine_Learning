7f7025773f653a9290e2e6afb9d185b31768076a
ofplarsen <ofplarsen@gmail.com>
2023-08-27 20:50:08 +0200

    Search tree is getting ca 90% and implemented correct. Need to finish print, and clean up code

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,7 +1,10 @@
-import numpy as np 
+import random
+
+import numpy as np
 import pandas as pd 
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
+
 
 
 class DecisionTree:
@@ -9,7 +12,35 @@ class DecisionTree:
     def __init__(self):
         # NOTE: Feel free add any hyperparameters 
         # (with defaults) as you see fit
-        pass
+        self.root = {}
+        self.sorted_names = []
+
+    def p(self, t, n):
+        return t/n
+
+    def entropy(self, S):
+        entropy = 0
+        for i in S:
+            p_i = self.p(i, sum(S))
+            if p_i == 0:
+                continue
+            entropy -= p_i * np.log2(p_i)
+
+        return entropy
+
+    def A(self, A, y):
+        A_y = pd.concat([A, y], axis=1)
+
+        split = A_y.groupby(A_y.columns[0])[A_y.columns[1]].value_counts().unstack(fill_value=0)
+        return [[count_yes, count_no] for count_yes, count_no in split.values] # Generalise more
+
+    def gain(self, S, A):
+        total = 0
+        for s_v in A:
+            #print(s_v)
+            total += sum(s_v) / sum(S) * self.entropy(s_v)
+
+        return self.entropy(S) - total
     
     def fit(self, X, y):
         """
@@ -21,9 +52,53 @@ class DecisionTree:
                 to the features.
             y (pd.Series): a vector of discrete ground-truth labels
         """
-        # TODO: Implement 
-        raise NotImplementedError()
-    
+        value_counts = y.value_counts()
+        if len(value_counts) == 1:
+            self.root['y'] = value_counts.index.tolist()[0]
+            return
+
+        entropy = self.entropy(value_counts.values)
+
+        dfs = []
+        A = []
+        gain = []
+        for c in X.columns:
+            #print(c)
+            A = self.A(X[c], y)
+            #print(A)
+
+            #print(self.gain(value_counts, A))
+            gain.append(self.gain(value_counts, A))
+
+        #print(gain)
+        #print(X.columns.tolist())
+
+        sorted_pairs = sorted(zip(gain, X.columns.tolist()), reverse=True)
+
+        # Unpack the sorted pairs into separate lists
+        sorted_names = [name for _, name in sorted_pairs]
+        #print(sorted_names)
+
+        self.sorted_names = sorted_names
+
+        X = X[sorted_names]
+
+        for index, i in enumerate(X.values):
+            tree = self.root
+
+            for x in i:
+                if x not in tree:
+                    tree[x] = {}
+                tree = tree[x]
+
+            if 'y' not in tree:
+                tree['y'] = y[index]
+
+
+
+        print(pd.Series(self.root))
+
+
     def predict(self, X):
         """
         Generates predictions
@@ -38,9 +113,20 @@ class DecisionTree:
         Returns:
             A length m vector with predictions
         """
-        # TODO: Implement 
-        raise NotImplementedError()
-    
+        y = []
+        X = X[self.sorted_names]
+        for index, i in enumerate(X.values):
+            tree = self.root
+            for x in i:
+                if x not in tree:
+                    x = random.choice(list(tree.keys()))
+                tree = tree[x]
+            y.append(tree['y'])
+
+        return np.array(y)
+
+
+
     def get_rules(self):
         """
         Returns the decision tree as a list of rules
@@ -59,9 +145,11 @@ class DecisionTree:
             ...
         ]
         """
-        # TODO: Implement
-        raise NotImplementedError()
-
+        rules = []
+        for key, value in self.root.items():
+            rules.append(([(key, value)], 'yes'))
+            print(f"Key: {key}, Value: {value}")
+        return rules
 
 # --- Some utility functions 
     
